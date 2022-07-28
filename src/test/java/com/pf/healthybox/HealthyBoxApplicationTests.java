e649cfa1c7576b52295e939ed72fd5a3924fb5b1
JIHYEON-NB <wlgus120332@gmail.com>
2022-07-28 13:36:11 +0900

    #4 -스프링 부트 시작하기 개발환경은 Spring Boot 2.7.2, Java 17, Intellij 2022.1.4 기술스펙은 이슈카드 또는  참조

---
+++ src/test/java/com/pf/healthybox/HealthyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pf.healthybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
