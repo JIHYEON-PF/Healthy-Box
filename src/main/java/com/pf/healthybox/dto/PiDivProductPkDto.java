30c93cbfeb24e992244d475916695e0739b11c9d
JIHYEON-NB <wlgus120332@gmail.com>
2022-08-09 12:21:51 +0900

    #15 - 품목 분류 관련 DTO 생성

---
+++ src/main/java/com/pf/healthybox/dto/PiDivProductPkDto.java
@@ -0,0 +1,21 @@
+package com.pf.healthybox.dto;
+
+import com.pf.healthybox.domain.productInformation.PiDivProduct;
+import com.pf.healthybox.domain.productInformation.PiDivProductPk;
+
+public record PiDivProductPkDto(String productGroup, String productCategory) {
+
+    public static PiDivProductPkDto of (String productGroup, String productCategory) {
+        return new PiDivProductPkDto(productGroup, productCategory);
+    }
+
+    // entity > dto
+    public static PiDivProductPkDto from(PiDivProductPk entity) {
+        return new PiDivProductPkDto(entity.getProductGroup(), entity.getProductGroup());
+    }
+
+    // dto > entity
+    public PiDivProductPk toEntity() {
+        return PiDivProductPk.of(productGroup, productCategory);
+    }
+}
