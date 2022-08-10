c69c0d93bddcc288e5d3281b7bb7d237ab94932d
JIHYEON-NB <wlgus120332@gmail.com>
2022-08-10 22:43:40 +0900

    #19 - Static Resource 명칭 변경으로 인해 기존 마크업 파일 수정 CSS파일의 명칭 변경과  마크업 완성으로 링크 연결

--- src/main/java/com/pf/healthybox/contoller/MyPageController.java
+++ src/main/java/com/pf/healthybox/contoller/MyPageController.java
@@ -2,13 +2,14 @@ package com.pf.healthybox.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/user")
+@RequestMapping("/mypage")
 @Controller
-public class MyPageController {
+public class MyPageController { //마이페이지 관련 페이지에 대한 컨트롤러(주문내역, 장바구니, 배송정보 관리 등)
 
-    @GetMapping("/mypage")
+    @GetMapping("/") //TODO: @PathVariable 을 통해 {id}를 입력받아 해당 아이디의 마이페이지 출력하게 변경
     public String showMyPage() {
         return "userTemplates/myPage";
     }
