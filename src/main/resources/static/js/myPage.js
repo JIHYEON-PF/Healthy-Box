2b3fb5ff46ddd5e236801779026dbea21d0ebc0b
JIHYEON-NB <wlgus120332@gmail.com>
2022-08-29 16:08:42 +0900

    #34 - 회원정보 수정을 위한 뷰 추가 회원의 개인정보수정을 위한 비밀번호 확인 뷰와 회원정보를 수정하기 위한 뷰 추가

---
+++ src/main/resources/static/js/myPage.js
@@ -0,0 +1,102 @@
+// 비밀번호 확인
+$(document).on("click", "#confirmBtn", function () {
+    let input_password = $('#input_pw').val();
+
+    if (isNotEmpty(input_password)) {
+        $.ajax({
+            url: "/api/mypage/check-pw/" + input_password,
+            method: "POST",
+            success: function (data) {
+                if (data) {
+                    location.replace("/mypage/modify-user");
+                } else {
+                    alert("비밀번호가 일치하지 않습니다.");
+                    $('#input_password').focus();
+                }
+            },
+            error: function (request, status, error) {
+                console.log("에러");
+                console.log(JSON.parse(request.responseText));
+            },
+            complete: function () {
+                console.log("완료");
+            }
+        })
+    } else if (isEmpty(input_password)) {
+        alert("비밀번호를 입력해주시기 바랍니다.");
+        $('#input_password').focus();
+    }
+});
+
+//// 회원정보 수정
+// 비밀번호 체크
+$(document).on("keyup", "#passwordConfirm", function () {
+    let input_password = $("#userPassword").val();
+    let input_confirmPw = $("#passwordConfirm").val();
+
+    if (input_password !== input_confirmPw) {
+        $('#wrongPw').removeAttr('hidden');
+    } else {
+        $('#wrongPw').attr('hidden','true');
+    }
+});
+
+// 회원정보 수정 전달
+$(document).on("click", "#modifyUserInfo", function () {
+    //데이터 입력 받은 후 JSON 으로 변환
+    let user_id = $("#userId").val();
+    let user_pw = $("#userPassword").val();
+    let user_phoneNumber = $("#phoneNumber").val();
+    let user_name = $("#userName").val();
+    let user_nickname = $("#nickname").val();
+    let user_email = $("#email").val();
+    let user_zipcode = $("#zipcode").val();
+    let user_address1 = $("#address1").val();
+    let user_address2 = $("#address2").val();
+
+    if ($('#wrongPw').attr('hidden') !== undefined) {
+        let input_data = {
+            'userId': user_id,
+            'userPw': user_pw,
+            'phoneNumber': user_phoneNumber,
+            'userName': user_name,
+            'nickname': user_nickname,
+            'email': user_email,
+            'zipcode': user_zipcode,
+            'address1': user_address1,
+            'address2': user_address2
+        }
+
+        let json = JSON.stringify(input_data);
+
+        $.ajax({
+            url: "/api/mypage/modify-user",
+            data: json,
+            method: "POST",
+            contentType: "application/json; charset=utf-8",
+
+            success: function (data) {
+                alert("회원정보 수정이 완료되었습니다.")
+                location.replace('/mypage/')
+            },
+            error: function (request, status, error) {
+                console.log("에러");
+                let errorLog = JSON.parse(request.responseText);
+                console.log(errorLog.message())
+            },
+            complete: function () {
+                console.log("완료");
+            }
+        });
+    }
+});
+
+//// 공통 함수
+const isEmpty=(str)=>{
+    let obj = String(str);
+    return obj === null || obj === undefined || obj === 'null' || obj === 'undefined' || obj === '';
+}
+
+const isNotEmpty=(str)=>{
+    return !isEmpty(str);
+}
