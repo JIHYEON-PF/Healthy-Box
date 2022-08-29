6b9aaaecd027bc128fb9fee2482323fdc60d4340
JIHYEON-NB <wlgus120332@gmail.com>
2022-08-29 16:06:58 +0900

    #34 - 회원의 개인정보를 수정하기 위한 api 추가

---
+++ src/main/java/com/pf/healthybox/contoller/apiController/MyPageApiController.java
@@ -0,0 +1,44 @@
+package com.pf.healthybox.contoller.apiController;
+
+import com.pf.healthybox.domain.baseInformation.BiUser;
+import com.pf.healthybox.dto.request.baseInformationReq.BiUserRequest;
+import com.pf.healthybox.dto.response.baseInformationRes.BiUserResponse;
+import com.pf.healthybox.service.BiUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/mypage")
+public class MyPageApiController {
+
+    private final BiUserService biUserService;
+
+    public MyPageApiController(@Autowired BiUserService biUserService) {
+        this.biUserService = biUserService;
+    }
+
+    @PostMapping("/check-pw/{pw}")
+    public Boolean checkPw(@PathVariable String pw, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        BiUser entity = (BiUser) session.getAttribute("loginUser");
+
+        return (entity != null && entity.getUserPw().equals(pw));
+    }
+
+    @PostMapping("/modify-user")
+    public void modifyUserInfo(@RequestBody BiUserRequest dataReq, HttpServletRequest sessionReq) {
+        biUserService.modifyUserInfo(dataReq.toDto());
+        HttpSession session = sessionReq.getSession();
+        session.setAttribute("loginUser", biUserService.showUserInfo(dataReq.userId()));
+    }
+
+}
