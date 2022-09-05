cdfcb430b9176f31a87d587b0ccb2e0940cfd94f
JIHYEON-NB <wlgus120332@gmail.com>
2022-09-05 09:25:23 +0900

    #33 - 도메인 클래스 수정 배송정보를 관리하는  클래스의 ENUM 타입 수정 및 도메인 컬럼 추가(배송지 명칭)

--- src/main/java/com/pf/healthybox/domain/config/DeliveryFlag.java
+++ src/main/java/com/pf/healthybox/domain/config/DeliveryFlag.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 public enum DeliveryFlag {
 
-    NEW("단건 배송 정보"),
-    DEF("기본 배송 정보");
+    DEF("기본 배송 정보"),
+    REG("저장 배송 정보");
 
     @Getter private final String description;
 
