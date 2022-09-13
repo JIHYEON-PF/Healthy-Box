a6f2ba08fe832a872b5df96b71e5eb1940eabc39
JIHYEON-NB <wlgus120332@gmail.com>
2022-09-14 01:37:42 +0900

    #31 - 주문 상세 내역 표현을 위한 Entity 수정 및 DTO 등 생성 `Entity`에 주문 관련 택배사와 운송장 번호 등 컬럼 추가됨 따라서 Dto 수정 및 디테일 정보를 내려주는 `res` 클래스 추가됨

---
+++ src/main/java/com/pf/healthybox/dto/orderInformationDto/OiOrderDetailDto.java
@@ -0,0 +1,33 @@
+package com.pf.healthybox.dto.orderInformationDto;
+
+import com.pf.healthybox.dto.response.orderInformationRes.OiOrderDetailResponse;
+
+import java.time.LocalDateTime;
+
+public record OiOrderDetailDto (
+        LocalDateTime createdAt,
+        String orderNo,
+        String status,
+        String productCode,
+        String productName,
+        int unitCost,
+        int qty,
+        int dcCost,
+        int amount,
+        String deliveryComp,
+        String deliveryCode,
+        String deliveryName,
+        String zipcode,
+        String address1,
+        String address2,
+        String payMethod,
+        int deliveryCost,
+        int totalDcCost,
+        int totalAmount
+) {
+
+    public static OiOrderDetailDto of(LocalDateTime createdAt, String orderNo, String status, String productCode, String productName, int unitCost, int qty, int dcCost, int amount, String deliveryComp, String deliveryCode, String deliveryName, String zipcode, String address1, String address2, String payMethod, int deliveryCost, int totalDcCost, int totalAmount) {
+        return new OiOrderDetailDto(createdAt, orderNo, status, productCode, productName, unitCost, qty, dcCost, amount, deliveryComp, deliveryCode, deliveryName, zipcode, address1, address2, payMethod, deliveryCost, totalDcCost, totalAmount);
+    }
+
+}
