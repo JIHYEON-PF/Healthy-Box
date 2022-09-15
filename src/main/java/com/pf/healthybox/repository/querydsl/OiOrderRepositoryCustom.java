198f0cbc322c7f9d41de459b8247ef73c1d9e15e
JIHYEON-NB <wlgus120332@gmail.com>
2022-09-16 00:48:09 +0900

    #31 - 새로 추가된 도메인에 대한 Repository 추가 어드민 환경설정을 위한 `Environment` 도메인에 대한 Repo와 해당 도메인의 쿼리를 조정하기 위한 `QueryDSL` 추가 주문 상태 변경의 `Repository`는 현재 데이터 저장에 대한 내역만 있으므로 기본 `JpaRepository`만 생성 주문 상태 변경과 판매자 코드를 가져오는 쿼리메소드 추가

--- src/main/java/com/pf/healthybox/repository/querydsl/OiOrderRepositoryCustom.java
+++ src/main/java/com/pf/healthybox/repository/querydsl/OiOrderRepositoryCustom.java
@@ -11,4 +11,10 @@ public interface OiOrderRepositoryCustom {
     List<Tuple> findOrderList(String userId);
 
     List<Tuple> findOrderDetail(String userId, String orderNo);
+
+    void deleteOrderDetail(String userId, String orderNo);
+
+    String findSellerCodeByOrderNo(String orderNo);
+
+    void updateStatusCode(String orderNo, String sellerCode, String statusCode);
 }
