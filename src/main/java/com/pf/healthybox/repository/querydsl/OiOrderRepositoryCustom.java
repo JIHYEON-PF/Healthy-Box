9879d50e69da2cadde7b1db4107fab3cad863ae1
JIHYEON-NB <wlgus120332@gmail.com>
2022-09-20 16:18:57 +0900

    #32 - 클래스에 불필요한 import 제거

--- src/main/java/com/pf/healthybox/repository/querydsl/OiOrderRepositoryCustom.java
+++ src/main/java/com/pf/healthybox/repository/querydsl/OiOrderRepositoryCustom.java
@@ -1,8 +1,6 @@
 package com.pf.healthybox.repository.querydsl;
 
-import com.pf.healthybox.domain.baseInformation.QBiPoint;
 import com.querydsl.core.Tuple;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 import java.util.List;
 
