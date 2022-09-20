7f470ea9ed48a8547aa2a5286c66ba520f7b1355
JIHYEON-NB <wlgus120332@gmail.com>
2022-09-20 16:22:51 +0900

    #32 - 배송정보 서비스 로직 레이어 추가

---
+++ src/main/java/com/pf/healthybox/service/OiBasketService.java
@@ -0,0 +1,56 @@
+package com.pf.healthybox.service;
+
+import com.pf.healthybox.dto.response.orderInformationRes.OiBasketListResponse;
+import com.pf.healthybox.repository.OiBasketRepository;
+import com.querydsl.core.Tuple;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class OiBasketService {
+
+    private final OiBasketRepository oiBasketRepository;
+
+    public OiBasketService(@Autowired OiBasketRepository oiBasketRepository) {
+        this.oiBasketRepository = oiBasketRepository;
+    }
+
+    //배송내역 조회
+    public List<OiBasketListResponse> returnBasketList(String userId) {
+        List<Tuple> tuples = oiBasketRepository.showBasketList(userId);
+        List<OiBasketListResponse> res = new ArrayList<>();
+
+        for (Tuple tuple : tuples) {
+            res.add(
+                    OiBasketListResponse.of(
+                            tuple.get(0, String.class),
+                            tuple.get(1, String.class),
+                            tuple.get(2, String.class),
+                            tuple.get(3, int.class),
+                            tuple.get(4, int.class),
+                            tuple.get(5, int.class)
+                    )
+            );
+        }
+        System.out.println("res = " + res);
+        return res;
+    }
+
+    public List<Integer> returnPriceInformation(String userId) {
+        List<Tuple> tuples = oiBasketRepository.showBasketPrice(userId);
+        System.out.println("tuples = " + tuples);
+        Integer amount = 0;
+        Integer deliveryCost = 0;
+        for (Tuple tuple : tuples) {
+            amount += tuple.get(0, Integer.class);
+            deliveryCost += tuple.get(1, Integer.class);
+        }
+
+        return List.of(amount, deliveryCost);
+    }
+}
