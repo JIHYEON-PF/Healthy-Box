caa36740293cff0bc28a02119129ca0da811612f
JIHYEON-NB <wlgus120332@gmail.com>
2022-10-09 10:50:14 +0900

    #96 - 주문 관련 비즈니스 로직 추가 주문의 등록과 주문과 관련된 금액 데이터를 가져오는 비즈니스 로직 추가

--- src/main/java/com/pf/healthybox/service/OiOrderService.java
+++ src/main/java/com/pf/healthybox/service/OiOrderService.java
@@ -2,12 +2,16 @@ package com.pf.healthybox.service;
 
 import com.pf.healthybox.domain.config.PayMethod;
 import com.pf.healthybox.domain.config.Status;
+import com.pf.healthybox.domain.productInformation.PiProduct;
+import com.pf.healthybox.dto.request.orderInformationReq.OiOrderRequest;
 import com.pf.healthybox.dto.request.orderInformationReq.OiOrderStatusContentRequest;
 import com.pf.healthybox.dto.response.orderInformationRes.OiOrderDetailResponse;
 import com.pf.healthybox.dto.response.orderInformationRes.OiOrderListResponse;
+import com.pf.healthybox.dto.response.orderInformationRes.OiOrderProductQtyResponse;
 import com.pf.healthybox.repository.EnvironmentRepository;
 import com.pf.healthybox.repository.OiOrderRepository;
 import com.pf.healthybox.repository.OiOrderStatusContentRepository;
+import com.pf.healthybox.repository.PiProductRepository;
 import com.querydsl.core.Tuple;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +19,23 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 @Service
 public class OiOrderService {
 
     private final OiOrderRepository oiOrderRepository;
+    private final PiProductRepository piProductRepository;
     private final EnvironmentRepository environmentRepository;
     private final OiOrderStatusContentRepository oiOrderStatusContentRepository;
 
-    public OiOrderService(@Autowired OiOrderRepository oiOrderRepository, EnvironmentRepository environmentRepository, OiOrderStatusContentRepository oiOrderStatusContentRepository) {
+    public OiOrderService(@Autowired OiOrderRepository oiOrderRepository,
+                          @Autowired PiProductRepository piProductRepository,
+                          @Autowired EnvironmentRepository environmentRepository,
+                          @Autowired OiOrderStatusContentRepository oiOrderStatusContentRepository) {
         this.oiOrderRepository = oiOrderRepository;
+        this.piProductRepository = piProductRepository;
         this.environmentRepository = environmentRepository;
         this.oiOrderStatusContentRepository = oiOrderStatusContentRepository;
     }
@@ -110,5 +118,66 @@ public class OiOrderService {
         if (!statusCode.equals("")) {
             oiOrderRepository.updateStatusCode(req.orderNo(), req.sellerCode(), statusCode);
         }
+    }
+
+    public void insertOrder(OiOrderRequest req) {
+        oiOrderRepository.save(req.toDto().toEntity());
+    }
+
+    public OiOrderProductQtyResponse findOrderInform(String productCode, String sellerCode, int qty) {
+        PiProduct product = piProductRepository.findProducts(productCode, sellerCode);
+        return OiOrderProductQtyResponse.of(product.getPiProductPk().getProductCode(),
+                product.getPiProductPk().getSellerCode(),
+                product.getProductName(),
+                product.getPrice(),
+                qty,
+                product.getDcCost(),
+                0);
+    }
+
+    public List<OiOrderProductQtyResponse> settingDeliveryCost(List<OiOrderProductQtyResponse> foundProduct) {
+        Set<String> sellerCodeSet = new HashSet<>();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        foundProduct.forEach((product) -> sellerCodeSet.add(product.sellerCode()));
+        sellerCodeSet.forEach((key) -> hashMap.put(key, 0));
+
+        for (OiOrderProductQtyResponse product : foundProduct) {
+            hashMap.put(product.sellerCode(), hashMap.get(product.sellerCode()) + ((product.unitCost() - product.dcCost()) * product.qty()));
+        }
+
+        for (int i = 0 ; i < foundProduct.size() ; i++) {
+            if (hashMap.containsKey(foundProduct.get(i).sellerCode()) && hashMap.get(foundProduct.get(i).sellerCode()) != 0) {
+                if (hashMap.get(foundProduct.get(i).sellerCode()) < 50000) {
+                    foundProduct.set(i, OiOrderProductQtyResponse.of(
+                            foundProduct.get(i).productCode(),
+                            foundProduct.get(i).sellerCode(),
+                            foundProduct.get(i).productName(),
+                            foundProduct.get(i).unitCost(),
+                            foundProduct.get(i).qty(),
+                            foundProduct.get(i).dcCost(),
+                            3000
+                    ));
+                    hashMap.replace(foundProduct.get(i).sellerCode(), 0);
+                }
+            }
+        }
+
+        return foundProduct;
+
+    }
+
+    public List<Integer> getOrderPrice(List<OiOrderProductQtyResponse> foundProduct) {
+        int totalUnitCost = 0;
+        int totalDeliverCost = 0;
+        int totalDcCost = 0;
+
+        for (OiOrderProductQtyResponse product : foundProduct) {
+            totalUnitCost += product.unitCost() * product.qty();
+            totalDeliverCost += product.deliveryCost();
+            totalDcCost += product.dcCost();
+        }
+
+        return List.of(totalUnitCost, totalDeliverCost, totalDcCost, totalUnitCost + totalDeliverCost - totalDcCost);
     }
 }
