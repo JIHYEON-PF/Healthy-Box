3dd7708e999ab5b601d9be6368071603ec0566bb
JIHYEON-NB <wlgus120332@gmail.com>
2022-10-18 22:20:55 +0900

    #110 - 정기배송과 관련된 백서비스 레이어 추가 정기배송 주문내역의 변경을 관리하는 `ApiController` 메서드 추가 관련 데이터를 DB에서 조회하고 활용할 수 있도록 서비스 로직과 레퍼지토릭 쿼리 메서트 수정

--- src/main/java/com/pf/healthybox/repository/querydsl/OiOrderRepositoryCustomImpl.java
+++ src/main/java/com/pf/healthybox/repository/querydsl/OiOrderRepositoryCustomImpl.java
@@ -6,10 +6,13 @@ import com.pf.healthybox.domain.orderInformation.OiOrder;
 import com.pf.healthybox.domain.orderInformation.QOiDeliver;
 import com.pf.healthybox.domain.orderInformation.QOiOrder;
 import com.pf.healthybox.domain.productInformation.QPiProduct;
+import com.pf.healthybox.domain.productInformation.QPiSubscribeProducts;
+import com.pf.healthybox.dto.orderInformationDto.OiOrderDetailDto;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
@@ -26,7 +29,7 @@ public class OiOrderRepositoryCustomImpl extends QuerydslRepositorySupport imple
     }
 
     @Override
-    public List<Tuple> findOrderList(String userId) {
+    public List<Tuple> findOrderList(String userId, String isSubscribe) {
 
         QOiOrder oiOrder = QOiOrder.oiOrder;
         QPiProduct piProduct = QPiProduct.piProduct;
@@ -37,10 +40,13 @@ public class OiOrderRepositoryCustomImpl extends QuerydslRepositorySupport imple
                         oiOrder.status.as("status"),
                         piProduct.productName.as("productName"),
                         oiOrder.count().as("productCnt"),
-                        (oiOrder.amount.subtract(oiOrder.dcCost).add(oiOrder.deliveryCost)).sum().as("amount"))
+                        (oiOrder.amount.subtract(oiOrder.dcCost).add(oiOrder.deliveryCost)).sum().as("amount"),
+                        oiOrder.isSubscribe.as("isSubscribe"),
+                        oiOrder.subscribeCode.as("subscribeCode"),
+                        oiOrder.deliveryDate.as("deliveryDate"))
                 .from(oiOrder)
                 .leftJoin(piProduct).on(oiOrder.productCode.eq(piProduct.piProductPk.productCode))
-                .where(oiOrder.userId.eq(userId))
+                .where(oiOrder.userId.eq(userId).and(oiOrder.isSubscribe.eq(isSubscribe)))
                 .groupBy(oiOrder.orderNo, oiOrder.createdAt, oiOrder.status)
                 .orderBy(oiOrder.createdAt.desc())
                 .fetch();
@@ -48,16 +54,39 @@ public class OiOrderRepositoryCustomImpl extends QuerydslRepositorySupport imple
     }
 
     @Override
-    public List<Tuple> findOrderDetail(String userId, String orderNo) {
+    public List<Tuple> findSubscribeOrderList(String userId, String isSubscribe) {
+
+        QOiOrder oiOrder = QOiOrder.oiOrder;
+        QPiSubscribeProducts subscribeProducts = QPiSubscribeProducts.piSubscribeProducts;
+
+        return queryFactory
+                .select(oiOrder.orderNo,
+                        oiOrder.createdAt,
+                        oiOrder.amount.sum().as("amount"),
+                        oiOrder.status,
+                        subscribeProducts.subscribe_name,
+                        oiOrder.deliveryDate.min().as("startDate"),
+                        oiOrder.deliveryDate.max().as("endDate"))
+                .from(oiOrder)
+                .leftJoin(subscribeProducts).on(oiOrder.subscribeCode.eq(subscribeProducts.subscribe_code).and(oiOrder.productCode.eq(subscribeProducts.product_code)))
+                .where(oiOrder.userId.eq(userId).and(oiOrder.isSubscribe.eq(isSubscribe)))
+                .groupBy(oiOrder.orderNo)
+                .orderBy(oiOrder.createdAt.desc())
+                .fetch();
+    }
+
+    @Override
+    public List<Tuple> findOrderDetail(String userId, String orderNo, String isSubscribe) {
 
         QOiOrder oiOrder = QOiOrder.oiOrder;
         QOiOrder subOiOrder = new QOiOrder("subOiOrder");
         QPiProduct piProduct = QPiProduct.piProduct;
         QOiDeliver oiDeliver = QOiDeliver.oiDeliver;
 
-        return queryFactory
+        JPAQuery<Tuple> query = queryFactory
                 .select(oiOrder.createdAt,
                         oiOrder.orderNo,
+                        oiOrder.orderIdx,
                         oiOrder.status,
                         oiOrder.productCode,
                         piProduct.productName,
@@ -84,16 +113,26 @@ public class OiOrderRepositoryCustomImpl extends QuerydslRepositorySupport imple
                                         .from(subOiOrder)
                                         .where(subOiOrder.orderNo.eq(oiOrder.orderNo)),
                                 "totalAmount"
-                            )
+                            ),
+                        oiOrder.cardName,
+                        oiOrder.quota,
+                        oiOrder.deliveryDate
                         )
                 .from(oiOrder)
                 .leftJoin(piProduct).on(oiOrder.productCode.eq(piProduct.piProductPk.productCode))
                 .leftJoin(oiDeliver).on(oiOrder.deliverIdx.eq(oiDeliver.idx))
                 .where(oiOrder.userId.eq(userId).and(oiOrder.orderNo.eq(orderNo)))
-                .groupBy(oiOrder.createdAt, oiOrder.orderNo, oiOrder.status, oiOrder.productCode, piProduct.productName, oiOrder.unitCost, oiOrder.qty, oiOrder.dcCost,
+                .groupBy(oiOrder.createdAt, oiOrder.orderNo, oiOrder.orderIdx, oiOrder.status, oiOrder.productCode, piProduct.productName, oiOrder.unitCost, oiOrder.qty, oiOrder.dcCost,
                         oiOrder.amount, oiOrder.deliveryComp, oiOrder.deliveryCode, oiDeliver.deliveryName,oiDeliver.zipcode, oiDeliver.address1, oiDeliver.address2, oiOrder.payMethod, oiOrder.deliveryCost, oiOrder.orderIdx)
-                .orderBy(oiOrder.orderIdx.asc())
-                .fetch();
+                ;
+
+        if (isSubscribe.equals("N")) {
+            query.orderBy(oiOrder.orderNo.asc());
+        } else if (isSubscribe.equals("Y")) {
+            query.orderBy(oiOrder.deliveryDate.asc());
+        }
+
+        return query.fetch();
     }
 
     @Override
@@ -130,4 +169,5 @@ public class OiOrderRepositoryCustomImpl extends QuerydslRepositorySupport imple
                 .where(oiOrder.orderNo.eq(orderNo).and(oiOrder.sellerCode.eq(sellerCode)))
                 .execute();
     }
+
 }
--- src/main/java/com/pf/healthybox/service/OiOrderService.java
+++ src/main/java/com/pf/healthybox/service/OiOrderService.java
@@ -2,12 +2,19 @@ package com.pf.healthybox.service;
 
 import com.pf.healthybox.domain.config.PayMethod;
 import com.pf.healthybox.domain.config.Status;
+import com.pf.healthybox.domain.orderInformation.OiOrder;
+import com.pf.healthybox.domain.orderInformation.OiOrderStatusContent;
 import com.pf.healthybox.domain.productInformation.PiProduct;
+import com.pf.healthybox.dto.orderInformationDto.OiOrderDetailDto;
+import com.pf.healthybox.dto.orderInformationDto.OiOrderStatusContentDto;
+import com.pf.healthybox.dto.orderInformationDto.OiOrderSubscribeListDto;
+import com.pf.healthybox.dto.request.orderInformationReq.OiOrderDetailRequest;
 import com.pf.healthybox.dto.request.orderInformationReq.OiOrderRequest;
 import com.pf.healthybox.dto.request.orderInformationReq.OiOrderStatusContentRequest;
 import com.pf.healthybox.dto.response.orderInformationRes.OiOrderDetailResponse;
 import com.pf.healthybox.dto.response.orderInformationRes.OiOrderListResponse;
 import com.pf.healthybox.dto.response.orderInformationRes.OiOrderProductQtyResponse;
+import com.pf.healthybox.dto.response.orderInformationRes.OiOrderSubscribeListResponse;
 import com.pf.healthybox.repository.EnvironmentRepository;
 import com.pf.healthybox.repository.OiOrderRepository;
 import com.pf.healthybox.repository.OiOrderStatusContentRepository;
@@ -17,8 +24,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 
 @Slf4j
@@ -41,8 +53,8 @@ public class OiOrderService {
     }
 
     @Transactional
-    public List<OiOrderListResponse> findOrderList(String userId) {
-        List<Tuple> tuples = oiOrderRepository.findOrderList(userId);
+    public List<OiOrderListResponse> findOrderList(String userId, String isSubscribe) {
+        List<Tuple> tuples = oiOrderRepository.findOrderList(userId, isSubscribe);
         List<OiOrderListResponse> res = new ArrayList<>();
 
         for (Tuple tuple : tuples) {
@@ -53,7 +65,10 @@ public class OiOrderService {
                             tuple.get(3, String.class),
                             tuple.get(4, Long.class),
                             tuple.get(5, int.class),
-                            tuple.get(2, Status.class).getDescription()
+                            tuple.get(2, Status.class).getDescription(),
+                            tuple.get(6, String.class),
+                            tuple.get(7, String.class),
+                            tuple.get(8, LocalDateTime.class)
                     )
             );
         }
@@ -61,8 +76,46 @@ public class OiOrderService {
         return res;
     }
 
-    public List<OiOrderDetailResponse> findOrderDetails(String userId, String orderNo) {
-        List<Tuple> tuples = oiOrderRepository.findOrderDetail(userId, orderNo);
+
+    public List<OiOrderSubscribeListResponse> findSubscribeOrderList(String userId, String isSubscribe) {
+        List<Tuple> tuples = oiOrderRepository.findSubscribeOrderList(userId, isSubscribe);
+        List<OiOrderSubscribeListResponse> res = new ArrayList<>();
+
+        String status = "";
+        Date today = new Date();
+
+        for (Tuple tuple : tuples) {
+            Date startDate = Date.from(tuple.get(5, LocalDateTime.class).atZone(ZoneId.systemDefault()).toInstant());
+            Date endDate = Date.from(tuple.get(6, LocalDateTime.class).atZone(ZoneId.systemDefault()).toInstant());
+
+            if (startDate.after(today)) {
+                status = "주문";
+            } else if (startDate.before(today) && endDate.after(today)) {
+                status = "진행중";
+            } else if (endDate.before(today)) {
+                status = "완료";
+            }
+
+            System.out.println("status = " + status);
+            System.out.println(Date.from(tuple.get(5, LocalDateTime.class).atZone(ZoneId.systemDefault()).toInstant()));
+
+            res.add(
+                    OiOrderSubscribeListResponse.of(
+                            tuple.get(0, String.class),
+                            tuple.get(1, LocalDateTime.class),
+                            tuple.get(2, int.class),
+                            status,
+                            tuple.get(4, String.class),
+                            tuple.get(5, LocalDateTime.class),
+                            tuple.get(6, LocalDateTime.class)
+                    )
+            );
+        }
+        return res;
+    }
+
+    public List<OiOrderDetailResponse> findOrderDetails(String userId, String orderNo, String isSubscribe) {
+        List<Tuple> tuples = oiOrderRepository.findOrderDetail(userId, orderNo, isSubscribe);
         List<OiOrderDetailResponse> res = new ArrayList<>();
 
         for (Tuple tuple : tuples) {
@@ -70,23 +123,27 @@ public class OiOrderService {
                     OiOrderDetailResponse.of(
                             tuple.get(0, LocalDateTime.class),
                             tuple.get(1, String.class),
-                            tuple.get(2, Status.class).getDescription(),
-                            tuple.get(3, String.class),
+                            tuple.get(2, int.class),
+                            tuple.get(3, Status.class).getDescription(),
                             tuple.get(4, String.class),
-                            tuple.get(5, int.class),
+                            tuple.get(5, String.class),
                             tuple.get(6, int.class),
                             tuple.get(7, int.class),
                             tuple.get(8, int.class),
-                            tuple.get(9, String.class),
+                            tuple.get(9, int.class),
                             tuple.get(10, String.class),
                             tuple.get(11, String.class),
                             tuple.get(12, String.class),
                             tuple.get(13, String.class),
                             tuple.get(14, String.class),
-                            tuple.get(15, PayMethod.class).getDescription(),
-                            tuple.get(16, int.class),
+                            tuple.get(15, String.class),
+                            tuple.get(16, PayMethod.class).getDescription(),
                             tuple.get(17, int.class),
-                            tuple.get(18, int.class)
+                            tuple.get(18, int.class),
+                            tuple.get(19, int.class),
+                            tuple.get(20, String.class),
+                            tuple.get(21, int.class),
+                            tuple.get(22, LocalDateTime.class)
                     )
             );
         }
@@ -106,6 +163,7 @@ public class OiOrderService {
     public void changeStatus(OiOrderStatusContentRequest req) {
         req = OiOrderStatusContentRequest.of(
                 req.orderNo(),
+                req.userId(),
                 oiOrderRepository.findSellerCodeByOrderNo(req.orderNo()),
                 req.division(),
                 req.title(),
@@ -179,5 +237,43 @@ public class OiOrderService {
         }
 
         return List.of(totalUnitCost, totalDeliverCost, totalDcCost, totalUnitCost + totalDeliverCost - totalDcCost);
+    }
+
+    @Transactional
+    public Boolean modifyDeliveryDate(String userId, String orderNo, int orderIdx, String deliveryDate) throws ParseException {
+
+        OiOrder entity = oiOrderRepository.findByUserIdAndOrderNoAndOrderIdx(userId, orderNo, orderIdx);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        if (entity != null) {
+            entity.setDeliveryDate(LocalDateTime.ofInstant(Instant.ofEpochMilli(sdf.parse(deliveryDate).getTime()), ZoneId.systemDefault()));
+        } else {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    @Transactional
+    public void changeSubscribeStatus(OiOrderStatusContentRequest req) {
+        OiOrderStatusContentDto dto = req.toDto();
+
+        List<OiOrder> entityList = oiOrderRepository.findByUserIdAndOrderNo(dto.userId(), dto.orderNo());
+
+        if (entityList != null) {
+            // 기존 주문 정보의 orderIdx 값을 가져와서 해당 주문 정보의 상태를 변경
+            for (OiOrder entity : entityList) {
+                if (!entity.getStatus().getDescription().equals("주문")) {
+                    entity.setStatus(Status.valueOf(dto.division()));
+                }
+            }
+
+            // 주문 상태 변경 후 사유 테이블 insert
+            oiOrderStatusContentRepository.save(dto.toEntity());
+        } else {
+            throw new EntityNotFoundException("해당 정보의 주문 정보가 존재하지 않습니다.");
+        }
     }
 }
