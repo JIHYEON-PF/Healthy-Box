3526c92c9929435e22a80087d9fdf5b8a5277858
JIHYEON-NB <wlgus120332@gmail.com>
2022-10-25 12:08:19 +0900

    #117 - 정기배송 장바구니 관련 서비스 로직 추가

--- src/main/java/com/pf/healthybox/service/OiBasketService.java
+++ src/main/java/com/pf/healthybox/service/OiBasketService.java
@@ -1,9 +1,11 @@
 package com.pf.healthybox.service;
 
-import com.pf.healthybox.domain.orderInformation.OiBasket;
+import com.pf.healthybox.domain.orderInformation.OiSubscribeBasket;
+import com.pf.healthybox.dto.orderInformationDto.OiSubscribeBasketDto;
 import com.pf.healthybox.dto.response.orderInformationRes.OiBasketListResponse;
+import com.pf.healthybox.dto.response.orderInformationRes.OiSubscribeBasketDetailResponse;
 import com.pf.healthybox.dto.response.orderInformationRes.OiSubscribeBasketListResponse;
-import com.pf.healthybox.dto.response.orderInformationRes.OiSubscribeBasketResponse;
+import com.pf.healthybox.dto.response.productInformationRes.PiSubscribeBasketDetailProductResponse;
 import com.pf.healthybox.repository.OiBasketRepository;
 import com.querydsl.core.Tuple;
 import lombok.extern.slf4j.Slf4j;
@@ -87,6 +89,66 @@ public class OiBasketService {
         }
 
         return res;
+    }
+
+    public List<OiSubscribeBasketDetailResponse> returnSubscribeBasketDetail(String userId, String basketNo) {
+         List<Tuple> tuples = oiBasketRepository.findByUserIdAndBasketNo(userId, basketNo);
+         List<OiSubscribeBasketDetailResponse> res = new ArrayList<>();
+         for (Tuple tuple : tuples) {
+             res.add(
+                     OiSubscribeBasketDetailResponse.of(
+                             tuple.get(0, String.class),        //basketNo
+                             tuple.get(1, LocalDateTime.class), //deliveryDate
+                             tuple.get(2, String.class),        //productCode
+                             tuple.get(3, String.class),        //productName
+                             tuple.get(4, int.class),           //productIdx
+                             tuple.get(5, int.class),           //qty
+                             tuple.get(6, String.class),        //sellerCode
+                             tuple.get(7, String.class),        //subscribeCode
+                             tuple.get(8, String.class),        //subscribeName
+                             tuple.get(9, int.class),           //price
+                             tuple.get(10, int.class),           //amount
+                             tuple.get(11, LocalDateTime.class),//startDate
+                             tuple.get(12, LocalDateTime.class) //endDate
+                     )
+             );
+         }
+        return res;
+    }
+
+    public List<PiSubscribeBasketDetailProductResponse> returnSubscribeProducts(String userId, String basketNo) {
+        List<Tuple> tuples = oiBasketRepository.findSubscribeDetailProducts(userId, basketNo);
+        List<PiSubscribeBasketDetailProductResponse> res = new ArrayList<>();
+
+        for (Tuple tuple : tuples) {
+            res.add(
+                    PiSubscribeBasketDetailProductResponse.of(
+                            tuple.get(0, String.class),     // productCode
+                            tuple.get(1, String.class),     // productName
+                            tuple.get(2, int.class)         // price
+                    )
+            );
+        }
+
+        return res;
+    }
+
+    public String findSubscribeCodeByUserIdAndBasketNo(String userId, String basketNo) {
+        return oiBasketRepository.findSubscribeCodeByUserIdAndBasketNo(userId, basketNo);
+    }
+
+    @Transactional
+    public boolean modifySubscribeBasket(OiSubscribeBasketDto dto) {
+        OiSubscribeBasket entity = oiBasketRepository.findByUserIdAndBasketNoAndProductIdx(dto.userId(), dto.basketNo(), dto.productIdx());
+        System.out.println("entity = " + entity);
+        if (entity != null) {
+            entity.setDeliveryDate(dto.deliveryDate());
+            entity.setProductCode(dto.productCode());
+            entity.setQty(dto.qty());
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
